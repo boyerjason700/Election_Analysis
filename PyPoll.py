349bf8449db462b5192c7deffdc74e9c162bcb4b
boyerjason700 <74840026+boyerjason700@users.noreply.github.com>
2021-07-09 21:02:18 -0700

    percentage calculation

--- PyPoll.py
+++ PyPoll.py
@@ -1,8 +1,8 @@
 # The data we need to retrieve.
-# 1. The total number of votes cast.
-# 2. A complete list of candiates who recieved votes.
+# 1. The total number of votes cast.-
+# 2. A complete list of candiates who recieved votes.-
 # 3. The percentage of votes each candiate won.
-# 4. The total number of votes each candiate won.
+# 4. The total number of votes each candiate won.-
 # 5. The winner of the election based on popular vote.
 
 import csv
@@ -17,6 +17,9 @@ total_votes = 0
 
 # Candidate Options
 candidate_options = []
+
+#Candidate Votes
+candidate_votes = {}
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -33,17 +36,36 @@ with open(file_to_load) as election_data:
         #Print the candidate name from each row
         candidate_name = row[2]
 
+        #If candidate name does not match any existing candidate...
         if candidate_name not in candidate_options:
 
-            #Append each candidate name to candidate list
+            #Add candidate name to candidate list
             candidate_options.append(candidate_name)
 
-#3. Print the total votes.
-print(total_votes)
+            #Begin tracking that candidates vote count.
+            candidate_votes[candidate_name] = 0
 
-#Print candidate list
-print(candidate_options)
+        #Add a vote to that candidates count.
+        candidate_votes[candidate_name] += 1
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+#1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    #3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: recieved {vote_percentage:.2f}% of the vote.")
+
+# #3. Print the total votes.
+# print(total_votes)
+
+# #Print candidate list
+# print(candidate_options)
+
+# #Print the candidate vote dictionary.
+# print(candidate_votes)
 
 
 
