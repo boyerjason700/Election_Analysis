0ab92068a681f4b4d08d4fb56eccf3dabec75a7d
boyerjason700 <74840026+boyerjason700@users.noreply.github.com>
2021-07-10 09:53:47 -0700

    step 5

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -65,16 +65,16 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_name not in county_options:
 
             # 4b: Add the existing county to the list of counties.
-
+            county_options.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[candidate_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
+        county_votes[candidate_name] += 1
 
 
 # Save the results to our text file.
